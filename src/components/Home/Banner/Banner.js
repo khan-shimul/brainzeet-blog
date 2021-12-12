a028be3e09db83d3176340e3b362a21c154baef0
khan-shimul <khanshimul196@gmail.com>
2021-12-13 02:55:19 +0600

    update banner

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,13 +1,27 @@
 import { Box, Container, createTheme, Grid, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import React from 'react';
 import RegularButton from '../../../StyledComponent/RegularButton';
 import Header from '../Header/Header';
 import './Banner.css';
+import bannerBg from '../../../images/banner/banner.jpg';
 
 const Banner = () => {
+    const useStyles = makeStyles({
+        root: {
+            background: `linear-gradient(359.72deg, rgba(72, 43, 231, 0.73) 0.25%, rgba(0, 0, 0, 0.23) 99.77%), url(${bannerBg})`,
+            height: '780px',
+            width: '100%',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }
+    });
+
+    const { root } = useStyles();
 
     return (
-        <Box component="section" className="banner">
+        <Box component="section" className={root}>
             <Container>
                 {/* <Header /> */}
                 <Grid
@@ -16,7 +30,7 @@ const Banner = () => {
                     justifyContent="center"
                     alignItems="center"
                     minHeight="100vh">
-                    <Grid item xs={12} md={8}>
+                    <Grid item xs={12} sm={12} md={8}>
                         <Typography variant="h3" sx={{ color: '#fff', fontWeight: 700, mb: 2 }}>
                             Startup <br /> Framework.
                             Suit <br /> Up Your Startup
@@ -29,7 +43,7 @@ const Banner = () => {
                             <RegularButton sx={{ background: 'transparent', color: '#fff', border: 1 }} >Learn More</RegularButton>
                         </Box>
                     </Grid>
-                    <Grid item xs={12} md={4}>
+                    <Grid item xs={0} sm={0} md={4}>
                     </Grid>
                 </Grid>
             </Container>
