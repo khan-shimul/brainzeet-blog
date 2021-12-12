d7f14ad1dd70c48c4e716d270e4e019aaf8effaa
khan-shimul <khanshimul196@gmail.com>
2021-12-13 03:21:33 +0600

    responsive text

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,25 @@
-import { Box, Container, createTheme, Grid, Typography } from '@mui/material';
+import { Box, Container, Grid } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import React from 'react';
 import RegularButton from '../../../StyledComponent/RegularButton';
 import Header from '../Header/Header';
 import './Banner.css';
 import bannerBg from '../../../images/banner/banner.jpg';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+const theme = createTheme();
+
+theme.typography.h1 = {
+    fontSize: '2.5rem',
+    '@media (min-width:600px)': {
+        fontSize: '0.5rem',
+    },
+    [theme.breakpoints.up('md')]: {
+        fontSize: '4rem',
+    },
+};
+
 
 const Banner = () => {
     const useStyles = makeStyles({
@@ -21,33 +36,35 @@ const Banner = () => {
     const { root } = useStyles();
 
     return (
-        <Box component="section" className={root}>
-            <Container>
-                {/* <Header /> */}
-                <Grid
-                    container
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    minHeight="100vh">
-                    <Grid item xs={12} sm={12} md={8}>
-                        <Typography variant="h3" sx={{ color: '#fff', fontWeight: 700, mb: 2 }}>
-                            Startup <br /> Framework.
-                            Suit <br /> Up Your Startup
-                        </Typography>
-                        <Typography variant="body1" sx={{ color: '#fff' }}>
-                            We have created a new product that will help designers, <br /> developers and companies create websites for their startups <br /> quickly and easily.
-                        </Typography>
-                        <Box component="div" sx={{ mt: 5 }}>
-                            <RegularButton sx={{ color: '#4a148c', mr: 2 }} >Learn More</RegularButton>
-                            <RegularButton sx={{ background: 'transparent', color: '#fff', border: 1 }} >Learn More</RegularButton>
-                        </Box>
+        <ThemeProvider theme={theme}>
+            <Box component="section" className={root}>
+                <Container>
+                    {/* <Header /> */}
+                    <Grid
+                        container
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        minHeight="100vh">
+                        <Grid item xs={12} sm={12} md={8}>
+                            <Typography variant="h1" sx={{ lineHeight: 1.2, color: '#fff', fontWeight: 700, mb: 2 }}>
+                                Startup <br /> Framework.
+                                Suit <br /> Up Your Startup
+                            </Typography>
+                            <Typography variant="body1" sx={{ color: '#fff' }}>
+                                We have created a new product that will help designers, <br /> developers and companies create websites for their startups <br /> quickly and easily.
+                            </Typography>
+                            <Box component="div" sx={{ mt: 5 }}>
+                                <RegularButton sx={{ color: '#4a148c', mr: 2 }} >Learn More</RegularButton>
+                                <RegularButton sx={{ background: 'transparent', color: '#fff', border: 1 }} >Learn More</RegularButton>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={0} sm={0} md={4}>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={0} sm={0} md={4}>
-                    </Grid>
-                </Grid>
-            </Container>
-        </Box>
+                </Container>
+            </Box>
+        </ThemeProvider>
     );
 };
 
--- src/components/Home/Promote/Promote.js
+++ src/components/Home/Promote/Promote.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+const theme = createTheme();
+
+theme.typography.h4 = {
+    fontSize: '1.2rem',
+    '@media (min-width:600px)': {
+        fontSize: '0.5rem',
+    },
+    [theme.breakpoints.up('md')]: {
+        fontSize: '2rem',
+    },
+};
+
+
 
 const Promote = () => {
+
     return (
-        <Box component="section" sx={{ my: 5 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                <Box component="div" sx={{ color: '#4a148c', fontSize: '25px', mr: 5 }}>
-                    <i className="fas fa-code"></i>
+        <ThemeProvider theme={theme}>
+            <Box component="section" sx={{ width: 2 / 4, mx: 'auto', my: 5 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <Box component="div" sx={{ color: '#4a148c', fontSize: '25px', mr: 5 }}>
+                        <i className="fas fa-code"></i>
+                    </Box>
+                    <Box component="div" sx={{ color: '#e91e63', mr: 5, fontSize: '25px' }}>
+                        <i className="fas fa-heart"></i>
+                    </Box>
+                    <Box component="div" sx={{ color: '#4a148c', fontSize: '25px' }}>
+                        <i className="far fa-star"></i>
+                    </Box>
                 </Box>
-                <Box component="div" sx={{ color: '#e91e63', mr: 5, fontSize: '25px' }}>
-                    <i className="fas fa-heart"></i>
-                </Box>
-                <Box component="div" sx={{ color: '#4a148c', fontSize: '25px' }}>
-                    <i className="far fa-star"></i>
+                <Box>
+                    <Typography variant='h4' sx={{ color: '#4a148c', textAlign: 'center', fontWeight: 700, mt: 3, mb: 1 }}>
+                        Better Landing for Your Startup
+                    </Typography>
+                    <Typography variant='body2' sx={{ color: '#757575', textAlign: 'center', lineHeight: 1.5 }}>
+                        We have created a new product that will help designers, developers and companies create websites for their startups quickly and easily.
+                    </Typography>
                 </Box>
             </Box>
-            <Box>
-                <Typography variant='h4' sx={{ color: '#4a148c', textAlign: 'center', fontWeight: 700, mt: 3, mb: 1 }}>
-                    Better Landing for Your Startup
-                </Typography>
-                <Typography variant='body2' sx={{ width: 2 / 4, color: '#757575', textAlign: 'center', mx: 'auto' }}>
-                    We have created a new product that will help designers, developers and companies create websites for their startups quickly and easily.
-                </Typography>
-            </Box>
-        </Box>
+        </ThemeProvider>
+
     );
 };
 
